3d440d9413336d30461b6f1a08cf82d757117e99
junayed-hassan <junayedhassan102045@gmail>
2024-10-07 23:24:23 +0600

    react project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-yellow-600 text-7xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, quo obcaecati beatae ducimus cumque magnam eius quae odio aspernatur nulla earum nisi necessitatibus fugit eum repellendus quidem ut consequatur! Cumque.</h1>
+    </>
+  )
+}
+
+export default App
